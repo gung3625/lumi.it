bbdb79dbe0f47ceb4e4deaf91dc89861f6d7dd42
gung3625 <gung3625@gmail.com>
2026-03-17 10:58:31 +0900

    Update list.js

--- list.js
+++ list.js
@@ -1,11 +1,24 @@
 import { getStore } from "@netlify/blobs";
 
-export const handler = async () => {
- const store = getStore({ name: 'lumi_vault' });
- const blobs = await store.list();
- const results = await Promise.all(blobs.map(async blob => {
- const item = await store.getMetadata(blob.key);
- return { key: blob.key, ...item.metadata };
- }));
- return { statusCode: 200, body: JSON.stringify(results) };
+export const handler = async (event) => {
+    const store = getStore({ 
+        name: 'lumi_vault',
+        siteID: "28d60e0e-6aa4-4b45-b117-0bcc3c4268fc", 
+        token: "nfp_bqKqY4GBrd8MNNLxCiCssFhRN5qGfzWe82f7"
+    });
+
+    try {
+        const list = await store.list();
+        const items = await Promise.all(list.blobs.map(async (blob) => {
+            const entry = await store.getMetadata(blob.key);
+            return { key: blob.key, metadata: entry.metadata };
+        }));
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(items)
+        };
+    } catch (err) {
+        return { statusCode: 500, body: err.message };
+    }
 };
