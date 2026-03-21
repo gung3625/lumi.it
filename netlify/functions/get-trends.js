de2dd3194c9ae3c39743af4ac6536d898ae46e58
gung3625 <gung3625@gmail.com>
2026-03-22 06:55:08 +0900

    Update get-trends.js

--- netlify/functions/get-trends.js
+++ netlify/functions/get-trends.js
@@ -175,3 +175,4 @@ exports.handler = async (event) => {
   // 4. 최종 기본값
   return { statusCode: 200, headers: corsHeaders, body: JSON.stringify({ category: storeKey, tags: DEFAULT_TRENDS[storeKey] || DEFAULT_TRENDS.other, updatedAt: null, source: 'default' }) };
 };
+ㅇㅇㅇㅇ
